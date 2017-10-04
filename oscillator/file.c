b46a8b88f75a9213744f2e27de2d69e5783c3898
aris997 <riccardo.riva.997@gmail.com>
2017-10-04 22:51:11 +0200

    lavori notturni

---
+++ oscillator/file.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main () {
+	int i, j;
+	FILE *output;
+	char nome[12];
+	for (i=0; i<5; i++) {
+		sprintf(name, "output%d2.")
+		output = fopen(nome, "w");
+		for (j=0; j<5; j++) {	fprintf(output, "Hello World!\n");	}
+		fclose(output);
+	}
+
+	exit(0);
+}

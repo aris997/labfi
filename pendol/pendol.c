86593c6ee5a4bee89b2bc56a602b0f637305cad4
aris997 <riccardo.riva.997@gmail.com>
2017-10-08 21:50:52 +0200

    salvataggio

--- pendol/pendol.c
+++ pendol/pendol.c
@@ -10,25 +10,34 @@ struct vector{
   double va;
 };
 
+struct  vectort{
+	double a;
+	double va;
+	double t;
+}
+
 struct vector euler(struct vector, double, double);
 struct vector cromer(struct vector, double, double);
+struct vectort older(struct vector, double);
 
 double acceleration(struct vector, double, double, double, double, double);
-double energy(struct vector, double);               
+double energy(struct vector, double);
+double period(struct vectort, struct vector, double);           
 
 int main(int argc, char *argv[]){
   
   int choice = 2, j;
   long int i, steps;                                             
-  double omega2, gamma, f0, omegaf, dt, tmax, e, e0, phi;
+  double omega2, gamma, f0, omegaf, dt, tmax, e, e0, phi, period;
   
   struct vector state;
+  struct vector stateold;
   
   FILE *input;
   FILE *output;
 
   input = fopen("input.dat", "r");
-  
+
   char filename[20];
   for (j=0; j<2; j++) {
 
@@ -47,7 +56,7 @@ int main(int argc, char *argv[]){
 	  	sprintf(filename, "p_euler%d.dat", j+1);
 	    output = fopen(filename, "w");          
 
-	    fprintf(output, "#t, a_i, va_i, E(t)/E0 - 1\n");
+	    fprintf(output, "#t, \ta_i, \tva_i, \tE(t)/E0 - 1\n");
 	    fprintf(output, "%lf %lf %lf %lf\n", 0., state.a, state.va, 0.);    
 	 
 	    for (i=0; i<steps; i++){
@@ -64,14 +73,16 @@ int main(int argc, char *argv[]){
 	  	sprintf(filename, "p_cromer%d.dat", j+1);
 	    output = fopen(filename, "w");
 
-	    fprintf(output, "#t, a_i, va_i, E(t)/E0 - 1\n");
+	    fprintf(output, "#t, \ta_i, \tva_i, \tE(t)/E0 - 1\n");
 	    fprintf(output, "%lf %lf %lf %lf\n", 0., state.a, state.va, 0.);    
 	 
-	    for (i=0; i<steps; i++){
+	    for (i=0; i<steps; i++) {
 	      phi = acceleration(state, omega2, gamma, f0, omegaf, dt * (i+1));
+	      stateold = older(state, dt * (i+1));
 	      state = cromer(state, dt, phi);
+	      period = period(stateold, state, dt);
 	      e = energy(state, omega2);
-	      fprintf(output, "%lf %lf %lf\t %lf\n", dt*(i+1), state.a, state.va, e/e0 - 1.);
+	      fprintf(output, "%lf %lf %lf\t %lf %lf\n", dt*(i+1), state.a, state.va, e/e0 - 1., period);
 	    }
 	  }
 
@@ -113,4 +124,26 @@ double acceleration(struct vector state, double omega2, double gamma, double f0,
 double energy(struct vector state, double omega2) {
 
   return 0.5 * state.va * state.va + omega2 * (1 - cos(state.a));
+}
+
+struct vectort older(struct vector state, double t) {
+
+	struct vertort stateold;
+
+	stateold.a = state.a;
+	stateold.va = state.va;
+	stateold.t = t;
+
+	return stateold;
+}
+
+
+double period(struct vector stateold, struct vector state, double dt) {
+
+	if ( stateold.a - state.a < 0 ) {
+		return stateold.t + stateold.a * staeteold.t * (stateold.a - state.a) / dt;
+	}
+	else {
+		return 0;
+	}
 }

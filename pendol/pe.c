31cbbd8cb9984bbbe78bf0e2f5673868cb19994d
aris997 <riccardo.riva.997@gmail.com>
2017-10-12 21:57:50 +0200

    aaa

--- pendol/pe.c
+++ pendol/pe.c
@@ -18,21 +18,17 @@ struct vector cromer(struct vector, double, double);
 double acceleration(struct vector, double, double, double, double, double);
 double energy(struct vector, double);
 
-//void period(struct vector, struct vector, double);
-
 int main(int argc, char *argv[]) {
 
 	int k;
 	double x0, v0, omega2, gamma, f0, omegaf, dt, tmax, e, e0, phi, Ta;
 	long int i, j, steps, n;
-	//char filename[20];
 	double tstar[1000] = {0};
 
 	struct vector state;
 	struct vector last;
 
 	FILE *input;
-	//FILE *output;
 
 	input = fopen("input.dat", "r");
 	printf("Cromer\n");
@@ -43,11 +39,6 @@ int main(int argc, char *argv[]) {
 
 	  steps = (long int)(tmax/dt);
 	  Ta = 2.*PI/sqrt(omega2);
-	  // sprintf(filename, "p_cromer%d.dat", k+1);
-	  // output = fopen(filename, "w");
-	  
-	  // fprintf(output, "#t, \ta_i, \tva_i, \tE(t)/E0 - 1\n");
-	  // fprintf(output, "%lf %lf %lf %lf\n", 0., state.a, state.va, 0.);
 
 	  for(j=0; j<3000; j++){
 	  	
@@ -71,10 +62,6 @@ int main(int argc, char *argv[]) {
 	  			printf("%lf %lf %lf %lf %.10lf\n", last.a, state.a, last.t, dt * (i+1), tstar[n]);
 	  			n++;
 	  		}
-
-		  	//e = energy(state, omega2);
-		  	
-		  	//fprintf(output, "%lf %lf %lf %lf\n", dt*(i+1), state.a, state.va, e/e0 - 1.);
 		  	
 		}
 
@@ -87,7 +74,6 @@ int main(int argc, char *argv[]) {
 
 		}
 
-	  //fclose(output);
 	}
 	fclose(input);
 
@@ -117,25 +103,3 @@ double acceleration(struct vector state, double omega2, double gamma, double f0,
   return - omega2 * sin(state.a) - gamma * state.va + f0 * cos(omegaf * t);
   
 }
-
-
-// void period(struct vector old, struct vector new, double omega2) {
-
-// 	double T, Ta;
-
-// 	Ta = 2*PI/sqrt(omega2);
-
-// 	if (old.a + new.a < 0 ) {
-  		
-//   		T = old.t + old.a * (old.t - new.t) / (new.a - old.a);
-
-//   		printf("%lf %lf %lf %lf %.10lf %.10lf\n", old.a, new.a, new.t, old.t, T, T/Ta - 1);
-  		
-//   		if (T/Ta - 1 > 0.1 ) {
-
-//   			printf("STOP: errore del periodo sopra il 10%% %lf \n", T/Ta - 1);
-//   			exit(0);
-
-//   		}
-//   	}
-// }
